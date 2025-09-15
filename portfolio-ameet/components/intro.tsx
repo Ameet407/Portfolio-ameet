e880daa5d1a8ac1422c1538f27f4513c22fb6d89
Ameet407 <ameetkumar119@gmail.com>
2025-09-15 23:41:04 +0530

    Update intro component

--- portfolio-ameet/components/intro.tsx
+++ portfolio-ameet/components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
         <p>
           <span className="font-bold">Hello, I'm Ameet Kumar Mishra.</span><br />
           I'm a <span className="font-bold">full-stack developer</span> 💻,
-          looking for <span className="font-bold">work opportunities</span> 👜 to learn and grow.
+          let's connect and build something together.
         </p>
 
 
